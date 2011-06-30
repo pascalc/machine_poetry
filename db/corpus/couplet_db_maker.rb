992b7919668e36ed37338cad456d9fd589aec55f
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2011-06-30 19:23:06 +0200

    Fixed indentation

--- db/corpus/couplet_db_maker.rb
+++ db/corpus/couplet_db_maker.rb
@@ -3,68 +3,68 @@ require_relative '../database_connect.rb'
 
 class Couplet_DB_Maker < Database_Connect
 
-	COUPLET_CORPUS = "couplet_corpus"
-	RHYMES = "rhymes"
-    RAND_LIMIT = 1000000
-	
-	private 
+  COUPLET_CORPUS = "couplet_corpus"
+  RHYMES = "rhymes"
+  RAND_LIMIT = 1000000
 
-	def make_tests
-		tests = []
+  private
 
-        # Line length
-		length = { :name => "length", :func => lambda { |line| line.length } }
-		tests << length
+  def make_tests
+    tests = []
 
-        # First word in line
-		first_word = { :name => "firstword", 
-			       :func => PoetryUtils.select_word(0) }
-		tests << first_word
+    # Line length
+    length = { :name => "length", :func => lambda { |line| line.length } }
+    tests << length
 
-        # Last word in line
-		last_word = { :name => "lastword", 
-			      :func => PoetryUtils.select_word(-1) }
-		tests << last_word
+    # First word in line
+    first_word = { :name => "firstword",
+                   :func => PoetryUtils.select_word(0) }
+    tests << first_word
 
-        # Number of syllables in line
-        syllables = { :name => "syllables",
+    # Last word in line
+    last_word = { :name => "lastword",
+                  :func => PoetryUtils.select_word(-1) }
+    tests << last_word
+
+    # Number of syllables in line
+    syllables = { :name => "syllables",
                   :func => lambda do |line|
-                            #  PoetryUtils.words(line).map {|w| PoetryUtils.syllables(w) }.inject(:+)
-                            PoetryUtils.syllables(line)
-                           end }
-        tests << syllables
+                    # PoetryUtils.words(line).map {|w| PoetryUtils.syllables(w) }.inject(:+)
+                    PoetryUtils.syllables(line)
+                  end }
+    tests << syllables
 
-        # Random number - used to select random line
-        random = { :name => "random", :func => lambda { |line| rand(RAND_LIMIT) } }
-        tests << random       
-	end
-	
-	def insert_corpus_row(line,tests,corpus) 
-		row = { "text" => line }
-		tests.each do |test|
-			row[test[:name]] = test[:func].call(line)
-		end
-		corpus.insert(row)
-	end
-	
-	public
+    # Random number - used to select random line
+    random = { :name => "random", :func => lambda { |line| rand(RAND_LIMIT) } }
+    tests << random
+  end
 
-	def make_db(filename)
-		tests = make_tests
-		file = File.open(filename)
-        database do |db|
-                db.drop_collection(COUPLET_CORPUS)
-                db.drop_collection(RHYMES)
-                
-                couplet_corpus = db[COUPLET_CORPUS]
-                couplet_corpus.create_index("firstword")
-                couplet_corpus.create_index("lastword")
-                couplet_corpus.create_index("syllables")
-                couplet_corpus.create_index("random")
+  def insert_corpus_row(line,tests,corpus)
+    row = { "text" => line }
+    tests.each do |test|
+      row[test[:name]] = test[:func].call(line)
+    end
+    corpus.insert(row)
+  end
 
-                rhymes = db[RHYMES]
-                
-                PoetryUtils.clean_lines(file) { |line| insert_corpus_row(line,tests,couplet_corpus) }
-        end
-	end
+  public
+
+  def make_db(filename)
+    tests = make_tests
+    file = File.open(filename)
+    database do |db|
+      db.drop_collection(COUPLET_CORPUS)
+      db.drop_collection(RHYMES)
+
+      couplet_corpus = db[COUPLET_CORPUS]
+      couplet_corpus.create_index("firstword")
+      couplet_corpus.create_index("lastword")
+      couplet_corpus.create_index("syllables")
+      couplet_corpus.create_index("random")
+
+      rhymes = db[RHYMES]
+
+      PoetryUtils.clean_lines(file) { |line| insert_corpus_row(line,tests,couplet_corpus) }
+    end
+  end
 end
